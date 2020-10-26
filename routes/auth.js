cee01c6856a13b048ab2bb60d756018419d143ef
Guido Perdomo <guidop91@gmail.com>
2020-10-26 10:24:38 -0400

    revamp password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 
 router.post('/register', async (req, res) => {
-  // Validate user
+  // Validate user register data
   const { error } = registerValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
--- validation.js
+++ validation.js
@@ -1,11 +1,35 @@
 const Joi = require('@hapi/joi');
 
+// Password regular expression to enforce:
+// - Length of 8-18 characters
+// - At least one capital letter
+// - At least one number
+// - At least one special character
+const lowercaseRE = /(?=.*?[a-z])/;
+const uppercaseRE = /(?=.*?[A-Z])/;
+const numberRE = /(?=.*?[0-9])/;
+const specialCharRE = /(?=.*?[!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])/;
+
 const userSchema = Joi.object({
   name: Joi.string().min(5).required(),
   email: Joi.string().email().min(6).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string()
+    .min(8)
+    .max(18)
+    .required()
+    .pattern(lowercaseRE, 'necessary one lowercase character')
+    .pattern(uppercaseRE, 'necessary one uppercase character')
+    .pattern(numberRE, 'necessary one number')
+    .pattern(specialCharRE, 'nessary one special char'),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().min(6).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const registerValidation = (userBody) => userSchema.validate(userBody);
+const loginValidation = (loginBody) => loginSchema.validate(loginBody);
 
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
